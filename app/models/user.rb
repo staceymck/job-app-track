bb5c1974e808ad71f5b5d50381762e6daa0834db
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-29 14:58:50 -0600

    Remove custom validation error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :username, presence: true, uniqueness: true { message: "Username taken. Please select another or login."}
-
+  validates :username, presence: true, uniqueness: true
+  
   has_many :job_apps
 end
