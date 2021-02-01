d71ca559c99030e7b0946c1d9882757dec6d4570
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-01 14:59:24 -0600

    Build out UsersController routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+
+  get '/signup' do
+    redirect '/job_apps' if logged_in?
+    erb :'/users/new'
+  end
+
+  post '/signup' do
+    user = User.new(username: params[:username], password: params[:password])
+    if user.save
+      session[:user_id] = user.id
+      redirect '/job_apps'
+    else
+      #message - not successful, try again
+      erb :'users/new'
+    end
+  end
+
+  get '/login' do
+    redirect '/job_apps' if logged_in?
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(password: params[:password])
+      session[:user_id] = user.id
+      redirect '/job_apps'
+    else
+      #message - "Login unsucccessful. Please login again or sign up if you don't have an account."
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
+  delete '/users/:id' do
+    redirect_if_not_logged_in
+    user = User.find_by(id: params[:id])
+    if user == current_user
+      user.destroy #does this delete all associated apps and follow ups?
+      session.clear
+      #success message?
+      redirect '/'
+    else
+      #not authorized message?
+      redirect '/'
+    end
+  end
+end
