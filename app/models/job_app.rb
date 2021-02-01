b1073b3a2051950ea5cdaccd9e34269772de3a12
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-01 12:37:05 -0600

    Add validations, enum values and FollowUp relationship

--- app/models/job_app.rb
+++ app/models/job_app.rb
@@ -1,3 +1,8 @@
 class JobApp < ActiveRecord::Base
+  validates :job_title, presence: true
+  enum app_status: {interested: 0, applied: 1, interview: 2, offer: 3, no_offer: 4, withdrawn: 5}
+  enum offer_decision: {na: 0, accepted: 1, declined: 2}
+
   belongs_to :user
+  has_many :follow_ups
 end
