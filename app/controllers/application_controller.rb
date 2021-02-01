e593a470a2099f92250963022f06dd2840401a23
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-01 16:49:18 -0600

    Enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') {SecureRandom.hex(64)}
   end
 
   get "/" do
