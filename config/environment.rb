c896d136a617ab92557d29ce0f2b4cfd4dbdfc59
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-01 16:48:33 -0600

    Require securerandom and load Dotenv

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,9 @@
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
+require 'securerandom'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+Dotenv.load if ENV['SINATRA_ENV'] == "development" #do I need 'if ENV....'? or just Dotenv.load
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
