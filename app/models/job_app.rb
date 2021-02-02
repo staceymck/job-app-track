073a2c03b27df7bbfe7f7a5194982e99fb5c5c97
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-02 10:54:40 -0600

    Validate presence of app_status

--- app/models/job_app.rb
+++ app/models/job_app.rb
@@ -1,5 +1,5 @@
 class JobApp < ActiveRecord::Base
-  validates :job_title, :company_name, presence: true
+  validates :job_title, :company_name, :app_status, presence: true
   enum app_status: {interested: 0, applied: 1, interview: 2, offer: 3, no_offer: 4, withdrawn: 5}
   enum offer_decision: {na: 0, accepted: 1, declined: 2}
 
