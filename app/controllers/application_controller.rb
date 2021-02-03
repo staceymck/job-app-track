74e5a413893002eaac7b4d439dbc7b5e4c2ecd2c
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-03 16:50:42 -0600

    Register Sinatra::Flash in configure block

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, ENV.fetch('SESSION_SECRET') {SecureRandom.hex(64)}
+    register Sinatra::Flash
   end
 
   get "/" do
