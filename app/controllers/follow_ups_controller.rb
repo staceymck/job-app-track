089c3f64605f8308e590105c9670033d433fba62
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-04 16:00:56 -0600

    Redirect to updated job app and use job_app.user to access follow_up's user

--- app/controllers/follow_ups_controller.rb
+++ app/controllers/follow_ups_controller.rb
@@ -2,10 +2,9 @@ class FollowUpsController < ApplicationController
 
   delete '/follow_up/:id' do
     @follow_up = FollowUp.find_by(id: params[:id])
-    if @follow_up && @follow_up.user == current_user 
+    if @follow_up && @follow_up.job_app.user == current_user 
       @follow_up.destroy
-      #message about successful delete
     end
-    redirect '/'
+    redirect "/job_apps/#{@follow_up.job_app.id}"
   end
 end
