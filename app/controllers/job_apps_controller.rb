3bc502b1579c119b970f91dedb9da70b34cabf95
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-04 15:51:40 -0600

    Mass assign to update follow-up action but don't include :delete hash

--- app/controllers/job_apps_controller.rb
+++ app/controllers/job_apps_controller.rb
@@ -72,16 +72,13 @@ class JobAppsController < ApplicationController
     if params[:saved_follow_ups]
       params[:saved_follow_ups].each do |details|
         follow_up = FollowUp.find_by(id: details[:id])
-        if details[:delete] && details[:delete] == "true"
+        if details[:delete] == "true"
           follow_up.destroy
         else
           details[:action_status] = "incomplete" if !details.keys.include?("action_status")
-            if !follow_up.update(
-              action: details[:action],
-              complete_by: details[:complete_by],
-              action_status: details[:action_status])
-              flash[:update_follow_up_error] = "Could not update follow-up action(s). Make sure to include an action and date."
-            end
+          if !follow_up.update(details.except(:delete))
+            flash[:update_follow_up_error] = "Could not update follow-up action(s). Make sure to include an action and date."
+          end
         end
       end
     end
