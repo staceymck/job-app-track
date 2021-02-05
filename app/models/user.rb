522ba586b590f3cedd93c4b7c6a0cbb8143161ab
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-05 11:24:23 -0600

    Add sectioning and adjust headings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :username, presence: true, uniqueness: true
+  #validates :password, length: { minimum: 6 }
   
   has_many :job_apps, dependent: :destroy
 end
