e0bd911ada00f138e26d413d5b5ca262e9958984
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-10 16:47:38 -0600

    Remove commented out validations

--- app/models/follow_up.rb
+++ app/models/follow_up.rb
@@ -1,12 +1,6 @@
 class FollowUp < ActiveRecord::Base
   validates :action, :complete_by, presence: true
-  # validate :complete_by_date_cannot_be_in_past
   enum action_status: {incomplete: 0, complete: 1}
 
   belongs_to :job_app
-
-  # def complete_by_date_cannot_be_in_past
-  #   errors.add(:complete_by, "can't be in the past") if
-  #     !complete_by.blank? and complete_by < Date.today
-  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :username, presence: true, uniqueness: true
-  #validates :password, length: { minimum: 6 }
   
   has_many :job_apps, dependent: :destroy
 end
