2f8dcf111456a951535da9aa00a6ef44633833e8
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-11 10:53:39 -0600

    Protect route + don't need instance variables for redirect

--- app/controllers/follow_ups_controller.rb
+++ app/controllers/follow_ups_controller.rb
@@ -1,10 +1,11 @@
 class FollowUpsController < ApplicationController
 
   delete '/follow_up/:id' do
-    @follow_up = FollowUp.find_by(id: params[:id])
-    if @follow_up && @follow_up.job_app.user == current_user 
-      @follow_up.destroy
+    redirect_if_not_logged_in
+    follow_up = FollowUp.find_by(id: params[:id])
+    if follow_up && follow_up.job_app.user == current_user 
+      follow_up.destroy
     end
-    redirect "/job_apps/#{@follow_up.job_app.id}"
+    redirect "/job_apps/#{follow_up.job_app.id}"
   end
 end
